7ac4b2072fd1086a154c0934366b751782fcdcdf
Kunyuku <136298955+Kunyuku@users.noreply.github.com>
2023-12-11 11:17:24 +0700

    Update Barudak1.sh

--- Barudak1.sh
+++ Barudak1.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt-get -y install systemctl && sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce docker-ce-cli containerd.io -y && sudo usermod -aG docker genzo2805
+sudo apt update && sudo apt install npm && sudo apt-get -y install systemctl && sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce docker-ce-cli containerd.io -y && sudo usermod -aG docker genzo2805
 
 npm install -g ccloli/DNSProxy
 git clone https://github.com/ccloli/DNSProxy.git && cd DNSProxy && npm install -g
